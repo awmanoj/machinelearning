2d6bd2f49bc59d48a2e440ee462df5d19c96386d
Manoj Awasthi <mawasthi@adobe.com>
2014-04-17 00:16:25 +0530

    Adding customary perceptron.

---
+++ perceptron.py
@@ -0,0 +1,21 @@
+from numpy import *
+
+def pcnfwd(inputs, weights):
+	activations = dot(inputs, weights)
+	activations = where(activations > 0, 1, 0)
+	return activations
+
+def pcntrain(inputs, targets, eta, iters):
+	inputs = concatenate((-ones((shape(inputs)[0],1)), inputs), axis=1) # <===
+	weights = initialweights(shape(inputs)[1], shape(inputs)[1])
+	for i in range(iters):
+		activations = pcnfwd(inputs, weights)
+		weights = weights + eta * dot(transpose(inputs), targets - activations)
+		print "Iteration: " + str(i+1)
+		print weights
+	return activations, weights
+	
+def initialweights(m,n):
+	return random.rand(m, n) * 0.1 - 0.05
+
+
