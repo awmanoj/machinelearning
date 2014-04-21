cbc94c774344962f425780a1ee74846a0422da26
Manoj Awasthi <mawasthi@adobe.com>
2014-04-21 23:21:51 +0530

    refactoring in linreg.

--- linearRegression.py
+++ linearRegression.py
@@ -1,6 +1,17 @@
 from numpy import *
 
-def linreg(inputs, targets):
+def train(inputs, targets):
 	inputs = concatenate((-ones((shape(inputs)[0],1)), inputs), axis = 1)
 	beta = dot(dot(linalg.inv(dot(transpose(inputs), inputs)), transpose(inputs)), targets)
+	return beta
+
+def classify(inputs, beta):
+	inputs = concatenate((-ones((shape(inputs)[0],1)), inputs), axis = 1)
 	print dot(inputs, beta)
+
+if __name__ == "__main__":
+	inputs = array(([[0,0],[0,1],[1,0],[1,1]]))
+	targets = array(([0],[1],[1],[1]))
+
+	beta = train(inputs, targets)
+	classify(inputs, beta)
